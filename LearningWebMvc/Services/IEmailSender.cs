cab99197d5b483cd33d09c27f5762c88f4340993
陈靖磊 <cjl2020@outlook.com>
2017-12-30 00:59:22 +0000

    Add project files.

---
+++ LearningWebMvc/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningWebMvc.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
